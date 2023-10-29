b3f2132cb713400777c5afd7321ad19b66281555
Draganek <Radek.Draganek@gmail.com>
2023-10-29 18:03:57 +0100

    68% forms

--- src/pages/Profile/ProfileDetails/ProfileDetails.js
+++ src/pages/Profile/ProfileDetails/ProfileDetails.js
@@ -1,15 +1,81 @@
+import { useEffect, useState } from "react"
+import LoadingButton from "../../../UI/LoadingButton/LoadingButton";
+import { validateEmail } from "../../../helpers/validations";
+
 export default function ProfileDetails() {
+
+    const [email, setEmail] = useState('radek.draganek@gmail.com');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [valid, setValid] = useState(null);
+    const [errors, setErrors] = useState({
+        email: '',
+        password: ''
+    })
+    const buttonDisabled = Object.values(errors).filter(x => x).length;
+
+    const submit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        setTimeout(() => {
+            //zapisywanie
+            if (true) {
+            } else {
+
+            }
+            setLoading(false)
+        }, 500)
+    }
+
+    useEffect(() => {
+        if (validateEmail(email)) {
+            setErrors({ ...errors, email: '' })
+        } else {
+            setErrors({ ...errors, email: 'Niepoprawny email' })
+        }
+    }, [email])
+
+    useEffect(() => {
+        if (password.length >= 4 || !password) {
+            setErrors({ ...errors, password: '' })
+        } else {
+            setErrors({ ...errors, password: 'Wymagane 4 znaki' })
+        }
+    }, [password])
+
     return (
-        <form>
+        <form onSubmit={submit}>
             <div className="form-group">
                 <label>Email</label>
-                <input type="email" value="Radek.Draganek@gmail.com" className="form-control"/>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    className={`form-control ${errors.email ? 'is-invalid' : 'is-valid'}`} />
+                <div className="invalid-feedback">
+                    {errors.email}
+                </div>
+                <div className="valid-feedback">
+                    Wszystko gra!
+                </div>
             </div>
             <div className="form-group">
                 <label>Hasło</label>
-                <input type="password" value="TajneHasło" className="form-control"/>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    className={`form-control ${errors.password ? 'is-invalid' : ''}`} />
+                <div className="invalid-feedback">
+                    {errors.password}
+                </div>
             </div>
-            <button className="btn btn-primary">Zapisz</button>
+            <LoadingButton
+                loading={loading}
+                disabled={buttonDisabled}>
+                Zapisz
+            </LoadingButton>
         </form>
     );
 }
