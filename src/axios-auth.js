ad7c6d434020f5567c1551bb8a0970ed6a8891ba
Draganek <Radek.Draganek@gmail.com>
2023-11-07 16:05:33 +0100

    deploy

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: 'https://identitytoolkit.googleapis.com/v1',
     params: {
-        'key': 'AIzaSyDER34xqMUtuQHjyY6QrGkbw4JqZ_WMiVI'
+        'key': process.env.REACT_APP_AUTH_KEY
     }
 });
 
